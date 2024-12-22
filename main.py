b3698522ba2616a4628403673e9de2db0ae39374
umutbide1 <umut.bide.564@gmail.com>
2024-12-23 02:49:49 +0300

    kütüphane tanımlamaları ve modul tanıtımları

---
+++ main.py
@@ -0,0 +1,10 @@
+# import_libraries.py
+import cv2
+import mediapipe as mp
+import os
+
+# MediaPipe modülleri ve araçları
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
