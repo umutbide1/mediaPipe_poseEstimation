bd52923d9ae5d0665b78bca93624e9f3d2be7cec
umutbide1 <umut.bide.564@gmail.com>
2024-12-23 02:51:55 +0300

    pose modeli hazır

--- main.py
+++ main.py
@@ -11,4 +11,12 @@ mp_drawing_styles = mp.solutions.drawing_styles
 video_folder = "./"  # Bu kodun bulunduğu dizin
 video_files = ["video1.mp4", "video2.mp4", "video3.mp4", "video4.mp4", "video5.mp4"]
 
+# MediaPipe Pose modelini tanımlama
+pose = mp_pose.Pose(
+    static_image_mode=False,
+    model_complexity=1,
+    enable_segmentation=False,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
 
