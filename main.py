7ba4dcbbd435d1399cd78b3516de4babfaff00cc
umutbide1 <umut.bide.564@gmail.com>
2024-12-23 02:51:30 +0300

    video listesi oluşturuldu aynı dizin yolu verildi

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# import_libraries.py
 import cv2
 import mediapipe as mp
 import os
 
-# MediaPipe modülleri ve araçları
+# MediaPipe Pose modülü ve çizim araçları
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+
+# İşlenecek video dosyalarının bulunduğu dizin ve isimleri
+video_folder = "./"  # Bu kodun bulunduğu dizin
+video_files = ["video1.mp4", "video2.mp4", "video3.mp4", "video4.mp4", "video5.mp4"]
+
 
